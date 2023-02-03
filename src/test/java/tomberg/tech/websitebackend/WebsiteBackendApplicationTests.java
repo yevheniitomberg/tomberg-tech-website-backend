0308faf0c5660f03b76e3a6c901b175f1cf2350e
Yevhenii Tomberg <yevhenii@tomberg.com>
2023-02-04 00:11:19 +0300

    First commit

---
+++ src/test/java/tomberg/tech/websitebackend/WebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tomberg.tech.websitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
